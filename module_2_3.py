c5b3e41b66df648bb2e5081ad3dfcdb4fde56fce
Vasek3824 <abat38@mail.ru>
2024-09-10 15:12:44 +0700

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+setter = 0
+while setter < len(my_list):
+    if my_list[setter] > 0:
+        print(my_list[setter])
+        setter += 1
+        if my_list[setter] == 0:
+            print(my_list[setter + 1])
+        continue
+    else:
+        break
+
