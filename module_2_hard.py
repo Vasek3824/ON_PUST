9aec9648549073154151ee4060a3ad6ae67cefcc
Vasek3824 <abat38@mail.ru>
2024-09-15 21:58:39 +0700

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -3,7 +3,7 @@ import random
 def parol():
     cod = random.randint(3, 20)
     for i in range(1, 21):
-        for j in range(2, 21):
+        for j in range(i+1, 21):
             if cod % (i + j) == 0:
                 if i >= j:
                     break
