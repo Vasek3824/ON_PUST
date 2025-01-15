34f9976190ab7f0c9f42a04156310c5d5c5d78ed
Vasek3824 <abat38@mail.ru>
2025-01-15 17:29:30 +0700

    Update module_13_4.py

--- module_13_4.py
+++ module_13_4.py
@@ -11,6 +11,16 @@ class UserState(StatesGroup):
         growth = State()
         weight = State()
 
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def all_massages(message):
+    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
 @dp.message_handler(text='Calories')
 async def set_age(message):
     await message.answer('Введите свой возраст: ')
